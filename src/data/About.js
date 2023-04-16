64a2a95c45eff6587db56ba5d46b5910e1ee1160
Dav.O <davolufuwa@gmail.com>
2023-04-16 11:22:39 +0100

    optimizing 17

--- src/data/About.js
+++ src/data/About.js
@@ -3,26 +3,27 @@ export const AboutUs = [
     id:1, 
     title:"Property Insurance",
     information:"We offer our customer property protection of liability coverage and insurance for their better life.",
-    imgUrl:require("./src/assets/icons-large/property_insurance.svg")
+    imgUrl:require("../assets/icons-large/property_insurance.svg")
   },
   {
     id:2,
     title:"Best Price",
     information:"Not sure what  you should be charging for your property? No need to worry, let us do the numbers for you.",
-    imgUrl:require("./src/assets/icons-large/best_price.svg")
+    // imgUrl:require("../src/assets/icons-large/best_price.svg")
+    imgUrl: require('../assets/icons-large/best_price.svg')
   },
   {
     id:3,
     title:"Registered Agents",
     information:"You no longer have to negotiate commissions and haggle with other agents it only cost 2%!",
-    imgUrl:require("./src/assets/icons-large/registered_agents.svg")
+    imgUrl:"./src/assets/icons-large/registered_agents.svg"
 
   },
   {
     id:4,
     title:"At Your Convenience",
     information:"Get a virtual tour, and schedule visits before you rent or buy any properties. You get overall control.",
-    imgUrl:require("./src/assets/icons-large/convenience.svg")
+    imgUrl:"./src/assets/icons-large/convenience.svg"
 
   }
 ]
