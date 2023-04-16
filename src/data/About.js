a72d6fdd7331e058db2e53ceba1dc2df875e3dbe
Dav.O <davolufuwa@gmail.com>
2023-04-16 12:52:38 +0100

    optimizing 20

--- src/data/About.js
+++ src/data/About.js
@@ -9,47 +9,7 @@ export const AboutUs = [
     id:2,
     title:"Best Price",
     information:"Not sure what  you should be charging for your property? No need to worry, let us do the numbers for you.",
-    // imgUrl:"./src/assets/icons-large/best_price.svg"
-    imgUrl:   <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="32" cy="32" r="31.5" fill="white" stroke="#00DEB6"/>
-    <circle cx="32" cy="32" r="28" fill="#0AC09F"/>
-    <rect x="40" y="40" width="24" height="24" rx="12" fill="#029278"/>
-    <path d="M52.0002 45.3335C48.3202 45.3335 45.3335 48.3202 45.3335 52.0002C45.3335 55.6802 48.3202 58.6668 52.0002 58.6668C55.6802 58.6668 58.6668 55.6802 58.6668 52.0002C58.6668 48.3202 55.6802 45.3335 52.0002 45.3335ZM49.8202 49.1068C50.3602 49.1068 50.8068 49.5468 50.8068 50.0935C50.8068 50.6335 50.3668 51.0802 49.8202 51.0802C49.2802 51.0802 48.8335 50.6402 48.8335 50.0935C48.8335 49.5468 49.2735 49.1068 49.8202 49.1068ZM49.9002 54.5335C49.8002 54.6335 49.6735 54.6802 49.5468 54.6802C49.4202 54.6802 49.2935 54.6335 49.1935 54.5335C49.0002 54.3402 49.0002 54.0202 49.1935 53.8268L53.5602 49.4602C53.7535 49.2668 54.0735 49.2668 54.2668 49.4602C54.4602 49.6535 54.4602 49.9735 54.2668 50.1668L49.9002 54.5335ZM54.1802 54.8935C53.6402 54.8935 53.1935 54.4535 53.1935 53.9068C53.1935 53.3668 53.6335 52.9202 54.1802 52.9202C54.7202 52.9202 55.1668 53.3602 55.1668 53.9068C55.1668 54.4535 54.7268 54.8935 54.1802 54.8935Z" fill="white"/>
-    <g clip-path="url(#clip0_1_1020)">
-    <g filter="url(#filter0_d_1_1020)">
-    <path d="M22.6666 44V22.6667C22.6666 21.9594 22.9476 21.2811 23.4477 20.781C23.9478 20.281 24.626 20 25.3333 20H38.6666C39.3739 20 40.0521 20.281 40.5522 20.781C41.0523 21.2811 41.3333 21.9594 41.3333 22.6667V44L37.3333 41.3333L34.6666 44L32 41.3333L29.3333 44L26.6666 41.3333L22.6666 44Z" fill="white"/>
-    <path d="M22.6666 44V22.6667C22.6666 21.9594 22.9476 21.2811 23.4477 20.781C23.9478 20.281 24.626 20 25.3333 20H38.6666C39.3739 20 40.0521 20.281 40.5522 20.781C41.0523 21.2811 41.3333 21.9594 41.3333 22.6667V44L37.3333 41.3333L34.6666 44L32 41.3333L29.3333 44L26.6666 41.3333L22.6666 44Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    </g>
-    <g filter="url(#filter1_d_1_1020)">
-    <path d="M34.6667 26.6667H31.3334C30.8029 26.6667 30.2942 26.8774 29.9192 27.2525C29.5441 27.6275 29.3334 28.1363 29.3334 28.6667C29.3334 29.1971 29.5441 29.7058 29.9192 30.0809C30.2942 30.456 30.8029 30.6667 31.3334 30.6667H32.6667C33.1971 30.6667 33.7058 30.8774 34.0809 31.2525C34.456 31.6275 34.6667 32.1363 34.6667 32.6667C34.6667 33.1971 34.456 33.7058 34.0809 34.0809C33.7058 34.456 33.1971 34.6667 32.6667 34.6667H29.3334M32 34.6667V36.6667M32 24.6667V26.6667" stroke="#029278" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" shape-rendering="crispEdges"/>
-    </g>
-    </g>
-    <defs>
-    <filter id="filter0_d_1_1020" x="19.6666" y="17" width="28.6667" height="34" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset dx="2" dy="2"/>
-    <feGaussianBlur stdDeviation="2"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.439216 0 0 0 0 0.396078 0 0 0 0 0.941176 0 0 0 0.1 0"/>
-    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_1020"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_1020" result="shape"/>
-    </filter>
-    <filter id="filter1_d_1_1020" x="26.3334" y="21.6667" width="15.3333" height="22" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-    <feOffset dx="2" dy="2"/>
-    <feGaussianBlur stdDeviation="2"/>
-    <feComposite in2="hardAlpha" operator="out"/>
-    <feColorMatrix type="matrix" values="0 0 0 0 0.439216 0 0 0 0 0.396078 0 0 0 0 0.941176 0 0 0 0.1 0"/>
-    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_1020"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_1020" result="shape"/>
-    </filter>
-    <clipPath id="clip0_1_1020">
-    <rect width="32" height="32" fill="white" transform="translate(16 16)"/>
-    </clipPath>
-    </defs>
-    </svg>  
+    imgUrl:"./src/assets/icons-large/best_price.svg"
   },
   {
     id:3,
