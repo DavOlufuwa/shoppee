9c05c74f59efa9d91ccfefd2e3bb529dac7c794c
Dav.O <davolufuwa@gmail.com>
2023-06-20 12:01:36 +0100

    Completed the Navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo  from '/assets/icons/logo-white.svg'
 
 const Navigation = () => {
+  const [navOpen, setNavOpen] = useState(false)
+  
+  
   return (
-    <div className='navigation-bar fixed top-0 w-full z-10 bg-background-secondary flex justify-between items-center px-10 py-4 shadow-md shadow-primary-brand-disabled'>
-      <div className='flex items-center w-36 cursor-pointer'>
-        <img src={Logo} alt='logo' className=''/>
+    <header>
+      <div className='nav-container'>
+        <div className='logo-box flex items-center w-36 cursor-pointer'>
+          <img src={Logo} alt='logo' className=''/>
+        </div>
       </div>
-      <nav className='flex gap-5'>      
-        <Link to="/" className='nav-link'>Explore</Link>
-        <Link to="/" className='nav-link'>Sign Up</Link>
-        <Link to="/" className='nav-link'>Sign In</Link>
-        <Link to="/" className='nav-link'>About</Link>
-        <Link to="/" className='nav-link'>Contact</Link>
+      <nav className={`nav-block ${navOpen ? "open" : " "}`}>      
+        <Link to="/" className="nav-link">Explore</Link>
+        <Link to="/" className="nav-link">Sign Up</Link>
+        <Link to="/" className="nav-link">Sign In</Link>
+        <Link to="/" className="nav-link">About</Link>
+        <Link to="/" className="nav-link">Contact</Link>
       </nav>
-      <div className='mobile-btn'><i className='material-icons'>expand_more</i></div>
-    </div>
+      <div 
+        className="mobile-btn"
+        onClick={() => setNavOpen(!navOpen)}
+      ><i className='material-icons'>expand_more</i>
+      </div>
+    </header>
   )
 }
 
